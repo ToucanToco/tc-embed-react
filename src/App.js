7bb1ae084dc1cc35f8b830ae0cc032dfb8636163
Jordan Lorho <jordan.lorho@gmail.com>
2020-04-14 17:11:08 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
+
+  componentDidMount() {
+    const script = document.createElement("script");
+
+    script.src = "https://demo-embed.toucantoco.com/scripts/embedLauncher.js?id=ba817dc8-e1e6-4ee0-8ac8-b461d52e1b20";
+    script.type = "text/javascript"
+    script.async = true;
+
+    document.querySelector('.embed-container').appendChild(script);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="embed-container"></div>
+      </div>
+    );
+  }
+};
 
 export default App;
